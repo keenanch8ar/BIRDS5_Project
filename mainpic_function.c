75228f4a88c8253d15fe85b07bf1e624e09096c4
YukihisaOtani <30889771+YukihisaOtani@users.noreply.github.com>
2021-07-15 15:03:23 +0900

    2021-7-15

--- mainpic_function.c
+++ mainpic_function.c
@@ -9,12 +9,19 @@ unsigned int32 address_data[4];
 unsigned int32 packet_data[2];
 unsigned int32 address;
 unsigned int32 packet;
-BYTE RTUC[8];
 BYTE Down[81];
-BYTE RTDC[9];
 BYTE dummy[1];
 BYTE Finish_sign[1];
 int i;
+<<<<<<< Updated upstream
+=======
+int result = 1;
+///////////////////////Uplink command Format////////////////////
+
+/*
+command[]
+*/
+>>>>>>> Stashed changes
 void Turn_On_CAM()
 {
    output_high (PIN_D7);
@@ -85,13 +92,13 @@ void PINO_Test()
          case 0x12:
          fprintf (fab, "Start 0x12\r\n") ;
          output_low (PIN_A5);
-         address_data[0] = command[1]<<24;
-         address_data[1] = command[2]<<16;
-         address_data[2] = command[3]<<8;
-         address_data[3] = command[4];
+         address_data[0] = command[2]<<24;
+         address_data[1] = command[3]<<16;
+         address_data[2] = command[4]<<8;
+         address_data[3] = command[5];
          address = address_data[0] + address_data[1] + address_data[2] + address_data[3];
-         packet_data[0] = command[5]<<8;
-         packet_data[1] = command[6];
+         packet_data[0] = command[6]<<8;
+         packet_data[1] = command[7];
          packet = (packet_data[0] + packet_data[1])*81;
 
          //fputc(command[5] + command[6], fab);
@@ -113,33 +120,33 @@ void PINO_Test()
          TRANSFER_DATA_NBYTE_TOFAB_SMF(address, command[5] + command[6]);
          fprintf (fab, "Finish 0x13\r\n") ;
          
-         break;*/
+         break;
+         */
          
          case 0x14://Uplink command to write the data on Flash Memory 2
          output_low (PIN_A5) ;//Main side
          fprintf (fab, "Start 0x14\r\n") ;
-         address_data[0] = command[1]<<24;
-         address_data[1] = command[2]<<16;
-         address_data[2] = command[3]<<8;
-         address_data[3] = command[4];
+         address_data[0] = command[2]<<24;
+         address_data[1] = command[3]<<16;
+         address_data[2] = command[4]<<8;
+         address_data[3] = command[5];
          address = address_data[0] + address_data[1] + address_data[2] + address_data[3];
          //sector_erase_SMF (address);
-         WRITE_DATA_BYTE_SMF (address, command[5]) ;
-         WRITE_DATA_BYTE_SMF (address + 1, command[6]) ;
-         WRITE_DATA_BYTE_SMF (address + 2, command[7]) ;
-         WRITE_DATA_BYTE_SMF (address + 3, command[8]) ;
+         WRITE_DATA_BYTE_SMF (address, command[6]) ;
+         WRITE_DATA_BYTE_SMF (address + 1, command[7]) ;
+         WRITE_DATA_BYTE_SMF (address + 2, command[8]) ;
          fprintf (fab, "Finish 0x14\r\n");
          break;
          
          case 0x16://Erase the data on Flash Memory 2
          output_low (PIN_A5);
          fprintf(fab, "Start 0x16\r\n");
-         address_data[0] = command[1]<<24;
-         address_data[1] = command[2]<<16;
-         address_data[2] = command[3]<<8;
-         address_data[3] = command[4];
+         address_data[0] = command[2]<<24;
+         address_data[1] = command[3]<<16;
+         address_data[2] = command[4]<<8;
+         address_data[3] = command[5];
          address = address_data[0] + address_data[1] + address_data[2] + address_data[3];
-            switch(command[5]){
+            switch(command[6]){
                case 0x04:
                   SUBSECTOR_4KB_ERASE_SMF(address);
                   fprintf(fab, "Finish 0x16\r\n");
@@ -152,6 +159,8 @@ void PINO_Test()
                   SECTOR_ERASE_SMF(address);
                   fprintf(fab, "Finish 0x16\r\n");
                   break;
+               default:
+                  fprintf(fab, "error\r\n");
             }
             break;
          
@@ -172,10 +181,14 @@ void PINO_Test()
          
          case 0x91://PINO Real Time Uplink Command
          fprintf (fab, "Start 0x91\r\n");
-         for (i = 0; i < 9; i++)
-         {
-            PINO_DATA[i] = command[i];
-         }
+         PINO_DATA[0] = command[0];
+         PINO_DATA[1] = command[2];
+         PINO_DATA[2] = command[3];
+         PINO_DATA[3] = command[4];
+         PINO_DATA[4] = command[5];
+         PINO_DATA[5] = command[6];
+         PINO_DATA[6] = command[7];
+         PINO_DATA[7] = command[8];
          
          for (i = 0; i<39; i++)
          {
@@ -186,10 +199,14 @@ void PINO_Test()
          
          case 0x92://PINO Real Time Downlink Command
          fprintf (fab, "Start 0x92\r\n") ;
-         for (i = 0; i < 9; i++)
-         {
-            PINO_data[i] = command[i];
-         }
+         PINO_DATA[0] = command[0];
+         PINO_DATA[1] = command[2];
+         PINO_DATA[2] = command[3];
+         PINO_DATA[3] = command[4];
+         PINO_DATA[4] = command[5];
+         PINO_DATA[5] = command[6];
+         PINO_DATA[6] = command[7];
+         PINO_DATA[7] = command[8];
          
          for (i = 0; i<39; i++)
          {
@@ -232,6 +249,7 @@ void PINO_Test()
          fprintf (fab, "Finish 0x94\r\n") ;
          break;
          
+         
          case 0x9B:
          fprintf (fab, "Start 0x9B\r\n") ;
          output_high (hvs);
@@ -240,22 +258,26 @@ void PINO_Test()
          output_low (sel);
          break;
          
-         case 0x9F://Time and attitude information
+         case 0x9C://Time and attitude information
          
          int a=0;
-         fprintf (fab, "Start 0x9F\r\n") ;
+         fprintf (fab, "Start 0x9C\r\n") ;
          
          for(a=0; a<12; a++){
             GET_RESET_DATA();
             delay_ms(10000);
          }
          
+<<<<<<< Updated upstream
          /*GET_RESET_DATA ();
          while(a<12){//Do "GET_RESET_DATA()" during 2mins;
             a = a + ten_sec_counter();
             GET_RESET_DATA ();
          }*/
          fprintf (fab, "Finish 0x9F\r\n") ;
+=======
+         fprintf (fab, "Finish 0x9C\r\n") ;
+>>>>>>> Stashed changes
          break;
          
 
@@ -301,7 +323,7 @@ void GET_RESET_DATA()                                                           
       dayh = reset_time_data[4];
       fprintf (fab, "\r\n") ;
       
-      PINO_DATA[0] = 0x81;
+      PINO_DATA[0] = 0xFC; //This the header for the Time attitude data.
       
       PINO_DATA[1] = reset_time_data[0];
       PINO_DATA[2] = reset_time_data[1];
@@ -314,12 +336,14 @@ void GET_RESET_DATA()                                                           
          PINO_DATA[i] = 0x01;
 
       }
+      fprintf(fab,"Send Data\r\n");
 
       for (j = 0; j < 39; j++)
       {
          //fprintf (fab, " % x, ", PINO_DATA[j]) ;
          
          fputc (PINO_DATA[j], DC);
+         
       }
       //fputc (Finish_sign[0], DC);
       
